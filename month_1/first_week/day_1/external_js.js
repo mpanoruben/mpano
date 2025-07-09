a3089b69891eb6279dfbb4f43584da8588ab886a
mpanoruben <mpanoruben@gmail.com>
2025-07-09 22:43:05 +0200

    step to running external.js and using terminal

---
+++ month_1/first_week/day_1/external_js.js
@@ -0,0 +1,7 @@
+
+let my_heading1 = document.getElementById("heading1");
+  let contaner = document.getElementById('doc')
+  contaner.innerHTML = 10000
+  my_heading1.innerHTML="welcome to js"
+  console.log(my_heading1)
+
