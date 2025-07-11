4dde2475de0ee83427163eeac333db38cf5e3867
mpanoruben <mpanoruben@gmail.com>
2025-07-11 11:14:20 +0200

    simple help you to div,sub,mutl and sub

---
+++ month_1/first_week/day_2/index.js
@@ -0,0 +1,21 @@
+let num1=2;
+let num2=5;
+let sum=num1+num2;
+console.log("the sum is", sum);
+
+
+let num4=6;
+let num5=2;
+let div=num4%num5;
+console.log("the div is", div);
+
+
+let num6=2;
+let num7=3;
+let mult=num6*num7;
+console.log("the mult is", mult);
+
+let num8=10;
+let num9=5;
+let sub=num8-num9;
+console.log("the sub is", sub);
