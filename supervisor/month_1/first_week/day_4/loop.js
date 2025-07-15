056060739b4aa8bf4267b5c55da4617b48508be4
MUHETO Hodal <mhthodol@gmail.com>
2025-07-15 11:44:31 +0200

    Add Loops for day 4

---
+++ supervisor/month_1/first_week/day_4/loop.js
@@ -0,0 +1,27 @@
+//  HOW TO WHILE LOOP
+// let i = 0;
+// sum = 0;
+// while (i < 20) {
+//     console.log("This is iteration number: " + i);
+//     i = i + 3;
+//     sum = sum + i;
+// }
+// console.log("Sum of all iterations: " + sum);
+
+// HOW TO DO FOR LOOP
+// let sum = 0;
+// for (let i = 0; i < 20; i=i+2) {
+//     console.log("This is iteration number: " + i);
+//     sum = sum + i;
+// }
+// console.log("Sum of all iterations: " + sum);
+
+// HOW TO DO DO WHILE LOOP
+let i = 0;
+let sum = 0;        
+do {
+    console.log("This is iteration number: " + i);
+    i = i + 2;
+    sum = sum + i;
+} while (i < 20);
+console.log("Sum of all iterations: " + sum);
