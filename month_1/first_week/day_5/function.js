791b033d92672f350ad90675b49a390415fc2c90
mpanoruben <mpanoruben@gmail.com>
2025-07-15 22:59:13 +0200

    function

---
+++ month_1/first_week/day_5/function.js
@@ -0,0 +1,11 @@
+// console.log ("welcome to timtom")
+
+function sayHello(){
+    let firstname ="mpano";
+    let lastname ="ruben";
+    console.log("WELCOME " + firstname + "  " + lastname + "");
+    console.log("")
+}
+sayHello();
+sayHello();
+sayHello();
---
+++ month_1/first_week/day_5/function_sum.js
@@ -0,0 +1,35 @@
+// function sumNumber(){
+//     let firstnumber= 5;
+//     let secondnumber= 4;
+//     let sum=firstnumber + secondnumber;
+//     console.log("sum is = " + firstnumber + " " + secondnumber + "");
+//     console.log("the sum is", sum);
+// }
+
+// sumNumber();
+
+
+// function sumNumber(a,b){
+//     let firstnumber= a;
+//     let secondnumber= b;
+//     let sum=firstnumber + secondnumber;
+//     console.log("sum is = " + firstnumber + " " + secondnumber + "");
+//     console.log("the sum is", sum);
+// }
+
+// sumNumber(49,80);
+// sumNumber(10,60);
+
+function checkName(x,y,z){
+    let firstname=x;
+    let lastname=y;
+    let age=z;
+    if (age < 30 ) {
+    console.log(" welcome mpano : " + "you are allowed to use it." );
+    }
+    else{
+    console.log("sorry mpano you are not allowed to use it.");
+}
+
+}
+checkName("mpano","ruben",20);
