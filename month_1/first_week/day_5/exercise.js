2fc279a2b1800a3140f0434d041909dae46d1c0f
mpanoruben <mpanoruben@gmail.com>
2025-07-24 23:24:09 +0200

    exercise_js

---
+++ month_1/first_week/day_5/exercise.js
@@ -0,0 +1,33 @@
+
+
+// function sayHello(){
+//     let firstname ="mpano";
+//     let lastname ="ruben";
+//     console.log("hello " + firstname + "  " + lastname + "");
+//     // console.log("")
+// }
+// sayHello();
+
+// let numA=2;
+// let numB=5;
+// let sum=numA+numB;
+// console.log("the sum is", sum);
+
+
+// global
+// let x = 10; 
+// function printX() {
+//   console.log(x); 
+// }
+
+// printX(); 
+// console.log(x); 
+
+// local
+function myFunction() {
+  let y = 5; // Local variable
+  console.log(y);
+}
+
+myFunction(); // Output: 5
+console.log(y); // ❌ Error: y is not defined
